5e2fff4d76d66f34082db178acba6717f12a4f10
momise92 <boss973@hotmail.fr>
2019-02-13 10:42:23 +0100

    Start

---
+++ src/main/java/com/capg/CapGApplication.java
@@ -0,0 +1,14 @@
+package com.capg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapGApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/capg/CapGApplicationTests.java
@@ -0,0 +1,17 @@
+package com.capg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapGApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
