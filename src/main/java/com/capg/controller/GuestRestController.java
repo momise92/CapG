a2d62330e592dfdecfacd54500fdab03ea332490
momise92 <boss973@hotmail.fr>
2019-02-14 12:23:15 +0100

    Update GuestRestController.java

--- src/main/java/com/capg/controller/GuestRestController.java
+++ src/main/java/com/capg/controller/GuestRestController.java
@@ -1,15 +1,20 @@
 package com.capg.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capg.dao.CityRepository;
+import com.capg.dao.DivisionsRepository;
 import com.capg.dao.RolesRepository;
 import com.capg.dao.UsersRepository;
 import com.capg.entities.City;
+import com.capg.entities.Divisions;
 import com.capg.entities.Roles;
 import com.capg.entities.Users;
 
@@ -23,13 +28,28 @@ public class GuestRestController {
 	RolesRepository rolesRepository;
 	@Autowired
 	CityRepository cityRepository;
+	@Autowired
+	DivisionsRepository divisionsRepository ;
 	
+	/*Inscription des users*/
 	@PostMapping(value = "/user")
 	public Users save(@RequestBody Users user) {
-		Roles defaultRole = rolesRepository.findByNameRole("espero");
+		Roles defaultRole = rolesRepository.findByNameRole("Association");
 		City yourCity = cityRepository.findByName(user.getCity().getName());
+		Divisions yourDivision = divisionsRepository.findByName(user.getDivisions().getName());
 		user.setRole(defaultRole);
 		user.setCity(yourCity);
+		user.setDivisions(yourDivision);
+		return userRepository.save(user);
+	}
+	
+	@PutMapping(value = "/user/{id}")
+	public Users update(@RequestBody Users user) {
+		City yourCity = cityRepository.findByName(user.getCity().getName());
+		Divisions yourDivision = divisionsRepository.findByName(user.getDivisions().getName());
+		user.setCity(yourCity);
+		user.setDivisions(yourDivision);
+		user.setLastUpdate(LocalDateTime.now());
 		return userRepository.save(user);
 	}
 
