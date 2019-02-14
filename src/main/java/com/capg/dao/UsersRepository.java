da11c70a50244230ecf791995621935dc40866f6
momise92 <boss973@hotmail.fr>
2019-02-14 18:34:57 +0100

    Add methods UserRepository

--- src/main/java/com/capg/dao/UsersRepository.java
+++ src/main/java/com/capg/dao/UsersRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.capg.entities.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Long>{
+	Users findByName(String name);
+	Users findByEmail(String email);
+	Users findByCity(String city);
+	Users findByDivisions(String division);
 
 }
