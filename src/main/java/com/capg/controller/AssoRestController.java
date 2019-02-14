99759e48a72afb6c2656fe2b8d1d9ae848f58b2c
hawa45 <hawaguapi@gmail.com>
2019-02-14 16:35:48 +0100

    Ajout controller Guest: - get ville - get division

--- src/main/java/com/capg/controller/AssoRestController.java
+++ src/main/java/com/capg/controller/AssoRestController.java
@@ -3,11 +3,19 @@ package com.capg.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capg.dao.DivisionsRepository;
+import com.capg.dao.ProjectsRepository;
 import com.capg.dao.UsersRepository;
+import com.capg.entities.Divisions;
+import com.capg.entities.Projects;
 import com.capg.entities.Users;
 
 @RestController
@@ -21,5 +29,28 @@ public class AssoRestController {
 	public List<Users> getAllUSers() {
 		return usersRepository.findAll();
 	}
+	
+	
+	@Autowired
+	ProjectsRepository projectsRepository;
+	
+	@GetMapping(value = "/projects")
+	public List<Projects> getAllProjects(){
+		return projectsRepository.findAll ();
+	}
+	
+	
+	
+	
+	
+
+	/*@PostMapping
+	public ResponseEntity<?> addProject(@RequestBody Projects projects) {
+
+	    if (projects.findByName(projects.getName()) != null) {
+	        return new ResponseEntity<String>("Il existe déja un projet de ce nom", HttpStatus.CONFLICT);
+	    }
+	    return new ResponseEntity<Projects>(projects.save(projects), HttpStatus.CREATED);
+	}*/
 
 }
