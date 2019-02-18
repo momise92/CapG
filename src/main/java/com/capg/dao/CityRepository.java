c9239d965f73105ee15d554745136d3d575dff6d
momise92 <boss973@hotmail.fr>
2019-02-18 23:24:32 +0100

    Create CityRepository.java

---
+++ src/main/java/com/capg/dao/CityRepository.java
@@ -0,0 +1,10 @@
+package com.capg.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capg.entities.City;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+	City findByName(String name);
+
+}
