8093474b9b4e4590ed441e7fb9d1705101b3f0ef
momise92 <boss973@hotmail.fr>
2019-02-18 23:25:03 +0100

    Create Project.java

---
+++ src/main/java/com/capg/entities/Project.java
@@ -0,0 +1,70 @@
+package com.capg.entities;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+@Entity
+@Data
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Setter(AccessLevel.NONE)
+	@Column(name = "project_id")
+	private Long id;
+	
+	private String name;
+	
+	@Column(columnDefinition = "TEXT", length= 255)
+	private String description;
+	
+	@Column(name="beginning_date")
+	private LocalDateTime beginningDate;
+	
+	@Column(name="end_date")
+	private LocalDateTime endDate;
+	
+	@OneToMany(mappedBy="project")
+	@JsonIgnore
+	private Set<Event> events = new HashSet<Event>();
+
+	
+	public Project () {}
+	
+	public Project(Long id, String name, String description, LocalDateTime beginningDate, LocalDateTime endDate) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.beginningDate = beginningDate;
+		this.endDate = endDate;
+	}
+	
+	public void addEvent(Event event)
+	{
+		this.events.add(event);
+	}
+	
+	public void removeEvent(Event event)
+	{
+		this.events.remove(event);
+	}
+	
+	/*public List<Event> getEvent()
+	{
+		return new ArrayList<Event>(events);
+	}*/
+
+}
