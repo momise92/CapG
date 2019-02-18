a08e2d83ed76f984a26cd36132cad904b1d8417b
momise92 <boss973@hotmail.fr>
2019-02-18 23:26:31 +0100

    Update Project.java

--- src/main/java/com/capg/entities/Project.java
+++ src/main/java/com/capg/entities/Project.java
@@ -9,9 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
