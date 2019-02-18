232f0982f22fb0fd2c201a3ec99055a378382efc
momise92 <boss973@hotmail.fr>
2019-02-18 23:22:46 +0100

    Delete EventsRepository.java

--- src/main/java/com/capg/dao/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capg.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.entities.Events;
-
-public interface EventsRepository extends JpaRepository<Events, Long>{
-
-}
