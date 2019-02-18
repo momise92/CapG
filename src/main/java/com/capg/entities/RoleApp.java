70da595dd3ec5731290510445430b2a9b8499630
momise92 <boss973@hotmail.fr>
2019-02-18 23:25:06 +0100

    Create RoleApp.java

---
+++ src/main/java/com/capg/entities/RoleApp.java
@@ -0,0 +1,29 @@
+package com.capg.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class RoleApp {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name_role")
+	@Size(min= 3, max= 50)
+	private String nameRole;
+	
+	protected RoleApp () {}
+	
+
+}
