a9f984b5e69fb9e11dcadb41306bcff15107eb79
hawa45 <hawaguapi@gmail.com>
2019-02-19 11:58:39 +0100

    add delete user to userAppController

--- src/main/java/com/capg/controller/UserAppController.java
+++ src/main/java/com/capg/controller/UserAppController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -81,5 +83,26 @@ public class UserAppController {
 			return new ResponseEntity<UserApp>(userAppRepository.save(user), HttpStatus.OK);
 		
 	}
+	
+	//User peut supprimer un user
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUserApp(@PathVariable Long id) {
+        ResponseEntity<?> result = null;
+
+        if (userAppRepository.findById(id) == null) {
+            return new ResponseEntity<String>("Ce user n'existe pas", HttpStatus.NOT_FOUND);}
+
+        try {
+            userAppRepository.deleteById(id);
+            result = new ResponseEntity<>(true,HttpStatus.OK);
+            }
+
+        catch (Exception ex) {
+
+            result = new ResponseEntity<String>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);}
+
+            return result;
+
+    }
 
 }
