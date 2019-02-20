2c60e5f0cd9e1fdf8342f182613fd23a94ffafce
momise92 <boss973@hotmail.fr>
2019-02-20 16:33:42 +0100

    Add relation ManytoMany with User

--- src/main/java/com/capg/entities/Event.java
+++ src/main/java/com/capg/entities/Event.java
@@ -1,6 +1,9 @@
 package com.capg.entities;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,7 +11,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AccessLevel;
@@ -26,9 +33,11 @@ public class Event {
 	@Column(name = "event_id")
 	private Long id;
 
+	@Column(name="name")
+	@Size(min=3, max=50)
 	private String name;
 
-	@Column(columnDefinition = "TEXT", length = 255)
+	@Column(columnDefinition = "TEXT")
 	private String description;
 
 	@Column(name = "beginning_date")
@@ -39,6 +48,10 @@ public class Event {
 
 	@Column(name = "place_number")
 	private int placeNumber;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy="events")
+	private Set<UserApp> users = new HashSet<>();
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "project_id")
