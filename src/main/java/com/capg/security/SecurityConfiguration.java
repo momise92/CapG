24128aa887fe705e0d6d52080459c49a47a97b41
momise92 <boss973@hotmail.fr>
2019-02-22 18:17:37 +0100

    Create SecurityConfiguration.java

---
+++ src/main/java/com/capg/security/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.capg.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Autowired
+	JWTFilter jwtFilter;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().and()
+		.cors().and()
+		.httpBasic().and()
+		.formLogin().disable()
+		.authorizeRequests()
+		.antMatchers("/", "/csrf").permitAll()
+		.antMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/city/**","/api/entities/**",
+				"/api/roles/**","/api/projects/**").permitAll()
+		.anyRequest().authenticated().and()
+		.addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+}
