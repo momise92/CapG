c07f22ca641151241d3ff5efcfad67ebc075e3e6
momise92 <boss973@hotmail.fr>
2019-02-22 18:16:05 +0100

    add exclude= UserDetailsService

--- src/main/java/com/capg/CapGApplication.java
+++ src/main/java/com/capg/CapGApplication.java
@@ -5,9 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class CapGApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
