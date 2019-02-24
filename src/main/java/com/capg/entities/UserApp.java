83275dfa313b810945bdc88e287f486136ac95eb
momise92 <boss973@hotmail.fr>
2019-02-25 00:33:33 +0100

    Add access=Access.WRITE_ONLY) for attribute password

--- src/main/java/com/capg/entities/UserApp.java
+++ src/main/java/com/capg/entities/UserApp.java
@@ -18,8 +18,8 @@ import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.AccessLevel;
 import lombok.Data;
@@ -46,8 +46,7 @@ public class UserApp {
 	@Column(nullable=false, unique=true)
 	private String email;
 	
-	@JsonIgnore
-	@Size(min=8, max=30)
+	@JsonProperty(access=Access.WRITE_ONLY)
 	@NotEmpty
 	private String password;
 	
