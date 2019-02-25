4a2af5a3c40b19b486db8e23bdf144c0a909ceb4
momise92 <boss973@hotmail.fr>
2019-02-25 11:58:25 +0100

    Update EntityCapController.java

--- src/main/java/com/capg/controller/EntityCapController.java
+++ src/main/java/com/capg/controller/EntityCapController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.capg.entities.EntityCap;
  */
 @RestController
 @RequestMapping("/api/")
+@Secured({"ROLE_association"})
 public class EntityCapController {
 	
 	@Autowired
