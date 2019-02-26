877c9e0e518ff538ea92a8704e44903b1740cc08
momise92 <boss973@hotmail.fr>
2019-02-26 01:18:15 +0100

    Enable Swagger

--- src/main/java/com/capg/CapGApplication.java
+++ src/main/java/com/capg/CapGApplication.java
@@ -6,13 +6,31 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class CapGApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapGApplication.class, args);
+	}
+	
+	@Bean
+	public Docket DocApi() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	        .select()
+	            .apis(RequestHandlerSelectors.basePackage("com.capg.controller"))
+	            .paths(PathSelectors.any())
+	        .build()
+	        .apiInfo(new ApiInfoBuilder().version("1.0").title("CapEngament").description("Documentation API v1.0").build());
 	}
 	
 	@Autowired
