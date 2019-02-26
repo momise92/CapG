a2c49dd631521ee4978141cbcc06db761bf8fffa
momise92 <boss973@hotmail.fr>
2019-02-26 17:33:37 +0100

    Update CapGApplication.java

--- src/main/java/com/capg/CapGApplication.java
+++ src/main/java/com/capg/CapGApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -15,6 +16,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
 @EnableSwagger2
 @SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class CapGApplication implements CommandLineRunner {
