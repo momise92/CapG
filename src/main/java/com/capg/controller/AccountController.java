4a40204bd79ff36027958651017879b1b3282133
momise92 <boss973@hotmail.fr>
2019-02-26 17:34:07 +0100

    Add register Controller and myAccount controller

--- src/main/java/com/capg/controller/AccountController.java
+++ src/main/java/com/capg/controller/AccountController.java
@@ -1,18 +1,27 @@
 package com.capg.controller;
 
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capg.dao.CityRepository;
+import com.capg.dao.EntityCapRepository;
+import com.capg.dao.RoleAppRepository;
 import com.capg.dao.UserAppRepository;
 import com.capg.dto.JsonCredential;
+import com.capg.dto.UserDto;
 import com.capg.entities.RoleApp;
 import com.capg.entities.UserApp;
 import com.capg.security.JWTService;
@@ -28,10 +37,18 @@ public class AccountController {
 	PasswordEncoder passwordEncoder;
 
 	@Autowired
+	RoleAppRepository roleAppRepository;
+	@Autowired
+	CityRepository cityRepository;
+	@Autowired
+	EntityCapRepository entityCapRepository;
+
+	@Autowired
 	JWTService jwtService;
 
 	@PostMapping(value = "/login")
-	public ResponseEntity<?> login(@RequestBody JsonCredential cred, HttpServletRequest request) throws Exception {
+	public ResponseEntity<?> login(@Valid @RequestBody JsonCredential cred, HttpServletRequest request)
+			throws Exception {
 
 		if (!userAppRepository.existsByEmail(cred.getEmail())) {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Bad user");
@@ -54,5 +71,42 @@ public class AccountController {
 		return ResponseEntity.ok().body(jwt);
 
 	}
+
+	@PostMapping(value = "/register")
+	public ResponseEntity<?> register(@Valid @RequestBody UserDto user) {
+		if (userAppRepository.existsByEmail(user.getEmail()))
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("user already exist");
+
+		if (!user.getPassword().equals(user.getCheckPassword()))
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Please check your password");
+
+		UserApp newUser = new UserApp();
+		newUser.setName(user.getName());
+		newUser.setLastName(user.getLastName());
+		newUser.setEmail(user.getEmail());
+		newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+		newUser.setPhoneNumber(user.getPhoneNumber());
+		newUser.setCreatedDate(LocalDateTime.now());
+		newUser.setStatus(roleAppRepository.findByNameStatus(user.getRole()));
+		newUser.setCity(cityRepository.findByName(user.getCity()));
+		newUser.setEntityCap(entityCapRepository.findByName(user.getEntityCap()));
+
+		return ResponseEntity.ok(userAppRepository.save(newUser));
+
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<UserApp> me() {
+		/*SecurityContextHolder.getContext().getAuthentication().getName();*/
+		
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		UserApp user = userAppRepository.findByEmail(name);
+
+		return ResponseEntity.ok(user);
+
+	}
+		
+		
 
 }
