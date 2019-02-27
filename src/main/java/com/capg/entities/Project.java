97fd405203de15df38ba3712d162c0c4675afa9c
momise92 <boss973@hotmail.fr>
2019-02-27 16:26:05 +0100

    Update Project.java

--- src/main/java/com/capg/entities/Project.java
+++ src/main/java/com/capg/entities/Project.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
@@ -15,47 +14,50 @@ import lombok.Setter;
 @Entity
 @Data
 public class Project {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Setter(AccessLevel.NONE)
 	@Column(name = "project_id")
 	private Long id;
-	
-	@Column(name="project_name", unique=true)
+
+	@Column(name = "project_name", unique = true)
 	private String name;
-	
-	@Column(columnDefinition = "TEXT", length= 255)
+
+	@Column(columnDefinition = "TEXT", length = 255)
 	private String description;
-	
-	@Column(name="start_date")
+
+	@Column(name = "start_date")
 	private LocalDateTime startDate;
-	
-	@Column(name="end_date")
+
+	@Column(name = "end_date")
 	private LocalDateTime endDate;
-	
-/*	@OneToMany( mappedBy = "project")
-	@JsonIgnoreProperties("project")
-	private Set<Event> events = new HashSet<Event>();*/
 
-	public Project () {}
-	
-/*	public void addEvent(Event event)
-	{
-		events.add(event);
-		event.setProject(this);
-	}
-	
-	public void removeEvent(Event event)
-	{
-		this.events.remove(event);
-		event.setProject(null);
-	}
-	
-	public List<Event>getEvents(){
-		return new ArrayList<Event>(events);
-	}*/
+	/*
+	 * @OneToMany( mappedBy = "project")
+	 * 
+	 * @JsonIgnoreProperties("project") private Set<Event> events = new
+	 * HashSet<Event>();
+	 */
 
-	
+	public Project() {
+	}
+
+	public Project(String name, String description, LocalDateTime startDate, LocalDateTime endDate) {
+		this.name = name;
+		this.description = description;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	/*
+	 * public void addEvent(Event event) { events.add(event);
+	 * event.setProject(this); }
+	 * 
+	 * public void removeEvent(Event event) { this.events.remove(event);
+	 * event.setProject(null); }
+	 * 
+	 * public List<Event>getEvents(){ return new ArrayList<Event>(events); }
+	 */
 
 }
